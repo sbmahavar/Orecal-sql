8ef9e7c731720ef3def35c4e7869f285833bed2b
shiva mahavar <sbmahavar@gmail.com>
2026-03-13 18:38:48 +0530

    Add initial employee records to Insert_data.sql
    Inserted multiple employee records into the emp table.

---
+++ Insert_data.sql
@@ -0,0 +1,32 @@
+-- 1. Inserting the first record
+INSERT INTO emp (emp_id, first_name, last_name, email, phone, join_date, salary)
+VALUES (1, 'Shiva', 'Mahavar', 'sbmahavar123@gmail.com', '+91 9227065381', '2024-05-02', 90000);
+
+-- 2. Inserting the second record
+INSERT INTO emp (emp_id, first_name, last_name, email, phone, join_date, salary)
+VALUES (2, 'Amit', 'Shah', 'amit10@gmail.com', '+91 9876538156', '2025-09-11', 52000);
+
+-- 3. Inserting the third record
+INSERT INTO emp (emp_id, first_name, last_name, email, phone, join_date, salary)
+VALUES (3, 'Yug', 'Patel', 'yug11@gmail.com', '+91 9225567891', '2025-09-12', 46000);
+
+-- Record 4
+INSERT INTO emp VALUES (4, 'Kunal', 'Mahta', 'Kunal212@gmail.com', '+91 9998888866', '13-Sep-2024', 55000);
+
+-- Record 5
+INSERT INTO emp VALUES (5, 'Deepak', 'Mahavan', 'mahavan@gmail.com', '+91 9889645445', '14-Sep-2025', 57000);
+
+-- Record 6
+INSERT INTO emp VALUES (6, 'Shivam', 'Vyas', 'shivam11@gmail.com', '+91 9227066656', '15-Sep-2025', 60000);
+
+-- Record 7
+INSERT INTO emp VALUES (7, 'Nikhil', 'Dave', 'nikhil21@gmail.com', '+91 88886663223', '15-Sep-2025', 70000);
+
+-- Record 8
+INSERT INTO emp VALUES (8, 'Jaypal', 'Jadeja', 'jaypal123@gmail.com', '+91 99966644455', '16-Sep-2025', 80000);
+
+-- Record 9
+INSERT INTO emp VALUES (9, 'Ankita', 'Yadav', 'yadav15@gmail.com', '+91 9895985638', '17-Sep-2025', 55000);
+
+-- Record 10
+INSERT INTO emp VALUES (10, 'Krushna', 'Boriya', 'krishna11@gmail.com', '+91 9696989899', '18-Sep-2025', 40000);
