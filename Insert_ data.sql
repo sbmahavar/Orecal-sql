2665eadc2c077fa854f71a11254b4da4464bf842
shiva mahavar <sbmahavar@gmail.com>
2026-03-15 08:23:23 +0530

    Create Insert_ data.sql

---
+++ Insert_ data.sql
@@ -0,0 +1,31 @@
+-- Entry 1
+INSERT INTO emp (Cemp_id, first_name, last_name, email, Phone, Join_date, Salary)
+VALUES (1, 'Shiva', 'Mahawar', 'shmahawar123@gmail.com', '+91 9227065381', '2024-05-02', 90000);
+
+-- Entry 2
+INSERT INTO emp (Cemp_id, first_name, last_name, email, Phone, Join_date, Salary)
+VALUES (2, 'Amit', 'Shah', 'amit10@gmail.com', '+91 9876538156', '2025-09-11', 52000);
+
+-- Entry 3
+INSERT INTO emp (Cemp_id, first_name, last_name, email, Phone, Join_date, Salary)
+VALUES (3, 'Yug', 'Patel', 'yug@gmail.com', '+91 9225567891', '2025-09-12', 48000);
+-- Record 4
+INSERT INTO emp VALUES (4, 'Kunal', 'Mahta', 'kunala12@gmail.com', '+91 9998885566', '13-Sep-2025', 55000);
+
+-- Record 5
+INSERT INTO emp VALUES (5, 'Deepak', 'Mahawar', 'mahavar@gmail.com', '+91 9889645451', '14-Sep-2025', 57000);
+
+-- Record 6
+INSERT INTO emp VALUES (6, 'Shivam', 'Vyas', 'shivamy1@gmail.com', '+91 9227066656', '15-Sep-2025', 60000);
+
+-- Record 7
+INSERT INTO emp VALUES (7, 'Nikhil', 'Dave', 'nikhild@gmail.com', '+91 8888666322', 70000);
+
+-- Record 8
+INSERT INTO emp VALUES (8, 'Jaypal', 'Jadeja', 'jaypalj22@gmail.com', '+91 9996664445', '16-Sep-2025', 80000);
+
+-- Record 9
+INSERT INTO emp VALUES (9, 'Ankita', 'Yadav', 'yadav15@gmail.com', '+91 9898985635', '17-Sep-2025', 55000);
+
+-- Record 10
+INSERT INTO emp VALUES (10, 'Krishna', 'Baniya', 'krishna11@gmail.com', '+91 9696989899', '18-Sep-2025', 90000);
