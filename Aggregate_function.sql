02abd707da572406869e347d8a59f6b32b6dad50
shiva mahavar <sbmahavar@gmail.com>
2026-03-19 08:57:07 +0530

    Create Aggregate_function.sql

---
+++ Aggregate_function.sql
@@ -0,0 +1,14 @@
+1. Count
+SELECT Count(emp-id) AS "Total Employee" From emp;
+
+2. MIN
+SELECT MIN(Salary) AS "Minimum Salary" From emp;
+
+3. MAX
+SELECT MAX(Salary) AS "MAXIMUM SALARY" From emp;
+
+4. AVG
+SELECT AVG(Salary) AS "Average Salary" From emp;
+
+5. SUM
+SELECT SUM(Salary) AS "Total Salary" From emp;
