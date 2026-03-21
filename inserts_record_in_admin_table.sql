12ac91bc3ff5c75f1cda489856e934a17a1c6d0d
shiva mahavar <sbmahavar@gmail.com>
2026-03-21 18:31:43 +0530

    Create inserts_record_in_admin_table.sql

---
+++ inserts_record_in_admin_table.sql
@@ -0,0 +1,16 @@
+/* Insert Record in admin Table */
+
+INSERT INTO admin (admin_id, emp_id, role, address) 
+VALUES (101, 1, 'System Admin', 'Rajkot');
+
+INSERT INTO admin (admin_id, emp_id, role, address) 
+VALUES (102, 2, 'HR Admin', 'Surat');
+
+INSERT INTO admin (admin_id, emp_id, role, address) 
+VALUES (103, 3, 'Application Admin', 'Vadodara');
+
+INSERT INTO admin (admin_id, emp_id, role, address) 
+VALUES (104, 4, 'Security Admin', 'Jamnagar');
+
+INSERT INTO admin (admin_id, emp_id, role, address) 
+VALUES (105, 5, 'Support Admin', 'Gandhinagar');
